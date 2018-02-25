ec24ed4c3e8618719ead15e2cafa0a06473c25b6
charlotteskinner90 <cs.skinner90@hotmail.co.uk>
2018-02-25 07:43:14 +0000

    Tidy up code and add JQuery function to button

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){ // Run this code when DOM is ready });
+
+    $(".btn").mouseenter(function(){
+
+        $(".btn").css("background-color","#ffffff");
+
+    });
+
+    $(".btn").mouseleave(function(){
+
+        $(".btn").css("background-color","#93af36");
+
+    });    
+
+  });
