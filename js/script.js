557d9ac594cc3aeea0cd1734ccfca5f494e33894
charlotteskinner90 <cs.skinner90@hotmail.co.uk>
2018-03-21 13:29:23 +0000

    Move CSS from HTML pages and into the style.css file

--- js/script.js
+++ js/script.js
@@ -1,16 +1,16 @@
-$(document).ready(function(){ // Run this code when DOM is ready });
+$(document).ready(function() { // Run this code when DOM is ready });
 
-	// Change colour of booking form submit button upon mouseenter, return to green when mouse leaves button //
+    // Change colour of booking form submit button upon mouseenter, return to green when mouse leaves button //
 
-    $(".btn").mouseenter(function(){
+    $(".btn").mouseenter(function() {
 
-        $(".btn").css("background-color","#ffffff");
+        $(".btn").css("background-color", "#ffffff");
 
     });
 
-    $(".btn").mouseleave(function(){
+    $(".btn").mouseleave(function() {
 
-        $(".btn").css("background-color","#93af36");
+        $(".btn").css("background-color", "#93af36");
 
     });
 
@@ -19,16 +19,14 @@ $(document).ready(function(){ // Run this code when DOM is ready });
     // Lightbox pop-up images //
 
     lightbox.option({
-      'resizeDuration': 300,
-      'wrapAround': true
+        'resizeDuration': 300,
+        'wrapAround': true
     })
 
     // Submit button confirmation of form submission //
-    
-	$(".btn").click(function() {
-	  alert("Thank you for your enquiry, we will be in touch!");
-	});
+
+    $(".btn").click(function() {
+        alert("Thank you for your enquiry, we will be in touch!");
+    });
 
 });
-
-
