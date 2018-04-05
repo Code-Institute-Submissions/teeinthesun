0a4bf5a055b049866c258691a97b6ff00fa656f9
charlotteskinner90 <cs.skinner90@hotmail.co.uk>
2018-04-05 14:16:36 +0100

    Split script.js file to form.js

---
+++ js/form.js
@@ -0,0 +1,21 @@
+    /* Date Picker - set so that arrival date cannot be in the past, 
+    and once arrival date chosen the departure date must be the same day or after */
+
+    $(document).ready(function() {
+        $(function() {
+            var dateToday = new Date();
+            var dates = $("#start-date, #end-date").datepicker({
+                defaultDate: "dateToday",
+                changeMonth: true,
+                numberOfMonths: 1,
+                minDate: dateToday,
+                onSelect: function(selectedDate) {
+                    var option = this.id == "start-date" ? "minDate" : "maxDate",
+                        instance = $(this).data("datepicker"),
+                        date = $.datepicker.parseDate(instance.settings.dateFormat ||
+                            $.datepicker._defaults.dateFormat, selectedDate, instance.settings);
+                    dates.not(this).datepicker("option", option, date);
+                }
+            });
+        });
+    });
