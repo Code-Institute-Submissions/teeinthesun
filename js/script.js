688e89b56dc82a3cd4e39398d025d28498593514
charlotteskinner90 <cs.skinner90@hotmail.co.uk>
2018-04-08 11:49:15 +0100

    Change mouseenter/mouseleave to css transition

--- js/script.js
+++ js/script.js
@@ -1,20 +1,5 @@
 $(document).ready(function() { // Run this code when DOM is ready });
 
-    /* Change colour of booking form submit button upon mouseenter, 
-    return to green when mouse leaves button */
-
-    $(".btn").mouseenter(function() {
-
-        $(".btn").css("background-color", "#ffffff");
-
-    });
-
-    $(".btn").mouseleave(function() {
-
-        $(".btn").css("background-color", "#93af36");
-
-    });
-
     // Lightbox pop-up images //
 
     lightbox.option({
